fc152e076cc7baa15ac843bc19d1b1c77c07bd0d
Leandro Oliveira <leandro.gooli@gmail.com>
2025-04-16 18:41:10 +0000

    Feat: container frontend

---
+++ investiq/frontend/app/page.tsx
@@ -0,0 +1,10 @@
+// frontend/pages/index.js
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Market AI - Next.js Frontend!</h1>
+    </div>
+  );
+}
