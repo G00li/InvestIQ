cd2456fd8870ea900684c1530abb20188ba3cb4f
Leandro Oliveira <leandro.gooli@gmail.com>
2025-04-16 17:49:03 +0000

    Setup inicial

--- investiq/app/main.py
+++ investiq/app/main.py
@@ -16,4 +16,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Market AI Backenddd for InvestIQ"}
+    return {"message": "Welcome to the Market AI Backend for InvestIQ"}
